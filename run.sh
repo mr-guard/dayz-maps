41e2230235091f4c9b95ff8241e24eb184015084
Senfo <lpsenfo@gmail.com>
2021-12-15 20:09:08 +0100

    Add more maps

--- run.sh
+++ run.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
+export MSYS_NO_PATHCONV=1;
+
+if [ -f ".env" ]; then
+    . .env
+fi;
+
 cmd=$1
 
 if [ "$cmd" = "" ]; then
-    echo "Using default cmd"
-    cmd='bash -c "cd /work && npm i && npm start"'
+    echo "Using default cmd chernarusplus"
+    cmd="chernarusplus"
 fi
 
-execution="
-docker run --rm \
-           -e STEAM_USER=\"$STEAM_USER\" \
-           -e STEAM_PASSWORD=\"$STEAM_PASSWORD\" \
-           -e STEAM_GUARD=\"$STEAM_GUARD\" \
-           -v `pwd`:/work \
-           -v `pwd`/gamedata/:/cache/dayzmaps/gamedata/ \
-           -v `pwd`/wsdata/:/cache/dayzmaps/wsdata/ \
-           -v `pwd`/extraction/:/cache/dayzmaps/extraction/ \
-           dayz-map-tools \
-           $cmd
-"
-eval $execution
+cmd="cd /cache/work && npm i && npm start $cmd"
+
+docker run -it --rm \
+    -e STEAM_USER="$STEAM_USER" \
+    -e STEAM_PASSWORD="$STEAM_PASSWORD" \
+    -e STEAM_GUARD="$STEAM_GUARD" \
+    -e FORCE_EXPORT="$FORCE_EXPORT" \
+    -e EXTRACTION_PATH="/p/extraction" \
+    -v "$(pwd):/cache/work" \
+    -v "$(pwd)/extraction:/p/extraction" \
+    -v "$(pwd)/gamedata/:/cache/dayzmaps/gamedata/" \
+    -v "$(pwd)/wsdata/:/cache/dayzmaps/wsdata/" \
+    dayz-map-tools \
+    bash -c "$cmd"
